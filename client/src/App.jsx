d6f0387ae6825c14da00e3c1295af89b325b125f
Acearoni <awoodson.contact@gmail.com>
2025-03-26 22:34:47 -0500

    DisplayAll component creation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,38 +1,16 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState} from 'react'
 import './App.css'
+import DisplayAll from './components/DisplayAll'
 
 function App() {
 
   const [characterList, setCharacterList] = useState([])
 
-  useEffect(() => {
-    axios.get('http://localhost:8000/api/allCharacters')
-      .then((response) => {
-        // console.log(response.data); //Will show the response data, which is an array
-        setCharacterList(response.data)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }, [])
-
-
   return (
     <>
       <h1>Full Stack</h1>
-      {
-        characterList.map((character) => (
-          <div>
-            <h2>Name: {character.name}</h2>
-            <h2>Class: {character.class}</h2>
-            <h2>Hometown: {character.hometown}</h2>
-            <h2>Level: {character.level}</h2>
-            <br></br>
-            {/* <h3>Created: {character.createdAt}</h3> */}
-          </div>
-        ))
-      }
+      <h2>Characters</h2>
+      <DisplayAll characterList={characterList} setCharacterList={setCharacterList} />
     </>
   )
 }
---
+++ client/src/components/DisplayAll.jsx
@@ -0,0 +1,38 @@
+import { useEffect} from 'react'
+import axios from 'axios'
+
+const DisplayAll = (props) => {
+const {characterList, setCharacterList} = props
+
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/allCharacters')
+            .then((response) => {
+                // console.log(response.data); //Will show the response data, which is an array
+                setCharacterList(response.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }, [])
+
+
+
+    return (
+        <div>
+            {
+                characterList.map((character) => (
+                    <div key={character._id}>
+                        <h2>Name: {character.name}</h2>
+                        <h2>Class: {character.class}</h2>
+                        <h2>Hometown: {character.hometown}</h2>
+                        <h2>Level: {character.level}</h2>
+                        <br></br>
+                        {/* <h3>Created: {character.createdAt}</h3> */}
+                    </div>
+                ))
+            }
+        </div>
+    );
+}
+
+export default DisplayAll;
